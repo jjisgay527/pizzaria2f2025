df8d582523877931b60b771705c3731942d9a34f
jjisgay527 <rm95193@estudante.fieb.edu.br>
2025-04-04 09:55:16 -0300

    Componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
 import { useState } from "react";
-
+import poke from './assets/pokeapi_256.png';
 function App() {
 
-  const [nome, setNome] = useState("memphis")
+  const Formulario = (props) => {
 
-  const Formulario = () => {
+    const [nome, setNome] = useState("memphis")
 
     return (
       <div>
       <input 
       className="nome"
       name= "nome"
-      onChange={(e)=>{nome=e.target.value}}
-      placeholder="Digite um nome"
+      onChange={(e)=>{setNome(e.target.value)}}
+      placeholder={props.sombra != null ? props.sombra : "Texto padrão"}
       type="text"/>
-      <button onClick={()=>{alert(nome)}}>
+
+      <button className="botao"
+      onClick={()=>{alert(nome)}}>
         CLIQUE AQUI
       </button>
-    </div>
-    
-      </div>
+
+    </div>    
     )
+  
   }
 
   return (
     <div>
       <h3>Pizzaria 2F</h3>
-      <Formulario />
+      <p>Texto de paragráfo</p>
+      <img src={poke} style={{witdh:200, height:100}}/>
+      <img src={poke} style={{witdh:200, height:100}}/>
+      <Formulario sombra="Digite seu nome..."/>
+      <Formulario sombra="Digite seu email..."/>
+      <Formulario sombra="(99)90000-0000"/>
       <Formulario />
       <Formulario />
 
